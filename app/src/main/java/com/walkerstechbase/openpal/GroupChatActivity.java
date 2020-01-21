a305c1bf8b1fa834c977c07404c5544210cf0a18
kojofosu <kojofosube@gmail.com>
2020-01-21 16:41:19 +0000

    can now send files in group chats

--- app/src/main/java/com/walkerstechbase/openpal/GroupChatActivity.java
+++ app/src/main/java/com/walkerstechbase/openpal/GroupChatActivity.java
@@ -1,7 +1,11 @@
 package com.walkerstechbase.openpal;
 
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.SweepGradient;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -17,6 +21,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
@@ -26,7 +32,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.github.angads25.toggle.interfaces.OnToggledListener;
 import com.github.angads25.toggle.model.ToggleableView;
 import com.github.angads25.toggle.widget.LabeledSwitch;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -35,6 +44,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 import com.vanniktech.emoji.EmojiEditText;
@@ -51,10 +65,14 @@ import java.util.Map;
 public class GroupChatActivity extends AppCompatActivity
 {
     private Toolbar mToolbar;
-    private ImageButton SendMessageButton, sendEmoji;
+    private ImageButton SendMessageButton, sendFilesBtn, sendEmoji;
     private EmojiEditText userMessageInput;
     //private ScrollView mScrollView;
     //private TextView displayTextMessages;
+    private String checker = "", myUrl = "";
+    private StorageTask uploadTask;
+    private ProgressDialog loadingBar;
+    private Uri fileUri;
 
     private FirebaseAuth mAuth;
     private DatabaseReference UsersRef, GroupNameRef, GroupMessageKeyRef;
@@ -93,7 +111,7 @@ public class GroupChatActivity extends AppCompatActivity
         GroupNameRef = FirebaseDatabase.getInstance().getReference().child("Groups").child(currentGroupName);
         RootRef = FirebaseDatabase.getInstance().getReference();
 
-
+        loadingBar = new ProgressDialog(this);
 
         Calendar calendar = Calendar.getInstance();
 
@@ -172,6 +190,47 @@ public class GroupChatActivity extends AppCompatActivity
 
 
 
+        sendFilesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence options[] = new CharSequence[] {"Images", "PDF Files", "Ms Word Files"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(GroupChatActivity.this);
+                builder.setTitle("Select the File");
+
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (i == 0){
+                            checker = "image";
+
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            intent.setType("image/*");
+                            startActivityForResult(Intent.createChooser(intent, "Select Image"), 438);
+                        }
+                        if (i == 1){
+                            checker = "pdf";
+
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            intent.setType("application/pdf");
+                            startActivityForResult(Intent.createChooser(intent, "Select PDF"), 438);
+
+                        }
+                        if (i == 2){
+                            checker = "docx";
+
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            intent.setType("application/msword");
+                            startActivityForResult(Intent.createChooser(intent, "Select Document"), 438);
+
+                        }
+                    }
+                });
+                builder.show();
+            }
+        });
 
 
 
@@ -248,58 +307,6 @@ public class GroupChatActivity extends AppCompatActivity
     }
 
 
-    private void SendMessage()
-    {
-        String messageText = userMessageInput.getText().toString();
-
-        if (TextUtils.isEmpty(messageText))
-        {
-            Toast.makeText(this, "first write your message...", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            String messageSenderRef = "Groups/"  + currentGroupName;
-            String messageReceiverRef = "Groups/" + currentGroupName ;
-
-
-            DatabaseReference userMessageKeyRef = RootRef.child("Groups")
-                    .child(currentGroupName).push();
-
-            String messagePushID = userMessageKeyRef.getKey();
-
-            Map messageTextBody = new HashMap();
-            messageTextBody.put("message", messageText);
-            messageTextBody.put("type", "text");
-            messageTextBody.put("from", currentUserID);
-            messageTextBody.put("fromName", currentUserName);
-            messageTextBody.put("to", currentGroupName);
-            messageTextBody.put("fromImage", currentUserImage);
-            messageTextBody.put("messageID", messagePushID);
-            messageTextBody.put("time", saveCurrentTime);
-            messageTextBody.put("date", saveCurrentDate);
-
-            Map messageBodyDetails = new HashMap();
-            messageBodyDetails.put(messageSenderRef + "/" + messagePushID, messageTextBody);
-            messageBodyDetails.put( messageReceiverRef + "/" + messagePushID, messageTextBody);
-
-            RootRef.updateChildren(messageBodyDetails).addOnCompleteListener(new OnCompleteListener() {
-                @Override
-                public void onComplete(@NonNull Task task)
-                {
-                    if (task.isSuccessful())
-                    {
-                        //Toast.makeText(GroupChatActivity.this, "Message Sent Successfully...", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        Toast.makeText(GroupChatActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                    }
-                    userMessageInput.setText("");
-                }
-            });
-        }
-    }
-
 
 //    @Override
 //    protected void onStart()
@@ -368,6 +375,7 @@ public class GroupChatActivity extends AppCompatActivity
         SendMessageButton = findViewById(R.id.send_message_button);
         userMessageInput = findViewById(R.id.input_group_message);
         sendEmoji = findViewById(R.id.send_emoji_btn);
+        sendFilesBtn = findViewById(R.id.send_files_btn);
 //        displayTextMessages = (TextView) findViewById(R.id.group_chat_text_display);
 //        mScrollView = (ScrollView) findViewById(R.id.my_scroll_view);
 
@@ -510,4 +518,202 @@ public class GroupChatActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 438 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            loadingBar.setTitle("Sending file");
+            loadingBar.setMessage("Please wait, file is sending...");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+
+            fileUri = data.getData();
+
+            if (!checker.equals("image")){
+                StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("document files");
+
+                final String messageSenderRef = "Groups/" + currentUserID + "/" + currentGroupName;
+                final String messageReceiverRef = "Groups/" + currentGroupName + "/" + currentUserID;
+
+                DatabaseReference userMessageKeyRef = RootRef.child("Groups")
+                        .child(currentUserID).child(currentGroupName).push();
+
+                final String messagePushID = userMessageKeyRef.getKey();
+
+                final StorageReference filesPath = storageReference.child(messagePushID + "." + checker);
+
+
+                filesPath.putFile(fileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        filesPath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                String downloadUrl = uri.toString();
+                                Map messageImageBody = new HashMap();
+
+                                messageImageBody.put("message", downloadUrl);
+                                messageImageBody.put("name", fileUri.getLastPathSegment());
+                                messageImageBody.put("type", checker);
+                                messageImageBody.put("from", currentUserID);
+                                messageImageBody.put("to", currentGroupName);
+                                messageImageBody.put("messageID", messagePushID);
+                                messageImageBody.put("time", saveCurrentTime);
+                                messageImageBody.put("date", saveCurrentDate);
+
+                                Map messageBodyDetails = new HashMap();
+                                messageBodyDetails.put(messageSenderRef + "/" + messagePushID, messageImageBody);
+                                messageBodyDetails.put( messageReceiverRef + "/" + messagePushID, messageImageBody);
+
+                                RootRef.updateChildren(messageBodyDetails);
+                                loadingBar.dismiss();
+                            }
+                        })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(GroupChatActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                    }
+                })
+                        .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                                double p = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                                loadingBar.setMessage((int) p + "% uploaded...");
+                            }
+                        });
+            }
+            else if (checker.equals("image")){
+                StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("image files");
+//
+//                final String messageSenderRef = "Groups/" + currentUserID + "/" + currentGroupName;
+//                final String messageReceiverRef = "Groups/" + currentGroupName + "/" + currentUserID;
+                String messageSenderRef = "Groups/"  + currentGroupName;
+                String messageReceiverRef = "Groups/" + currentGroupName ;
+
+                DatabaseReference userMessageKeyRef = RootRef.child("Groups")
+                        .child(currentUserID).child(currentGroupName).push();
+
+                final String messagePushID = userMessageKeyRef.getKey();
+
+                final StorageReference filesPath = storageReference.child(messagePushID + "." + "jpg");
+
+                uploadTask = filesPath.putFile(fileUri);
+
+                uploadTask.continueWithTask(new Continuation() {
+                    @Override
+                    public Object then(@NonNull Task task) throws Exception {
+                        if (!task.isSuccessful()){
+                            throw  task.getException();
+                        }
+                        return filesPath.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>(){
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()){
+                            Uri downloadUrl = task.getResult();
+                            myUrl = downloadUrl.toString();
+
+                            Map messageImageBody = new HashMap();
+                            messageImageBody.put("message", myUrl);
+                            messageImageBody.put("name", fileUri.getLastPathSegment());
+                            messageImageBody.put("type", checker);
+                            messageImageBody.put("from", currentUserID);
+                            messageImageBody.put("fromName", currentUserName);
+                            messageImageBody.put("to", currentGroupName);
+                            messageImageBody.put("messageID", messagePushID);
+                            messageImageBody.put("time", saveCurrentTime);
+                            messageImageBody.put("date", saveCurrentDate);
+
+                            Map messageBodyDetails = new HashMap();
+                            messageBodyDetails.put(messageSenderRef + "/" + messagePushID, messageImageBody);
+                            messageBodyDetails.put( messageReceiverRef + "/" + messagePushID, messageImageBody);
+
+                            RootRef.updateChildren(messageBodyDetails).addOnCompleteListener(new OnCompleteListener() {
+                                @Override
+                                public void onComplete(@NonNull Task task)
+                                {
+                                    if (task.isSuccessful())
+                                    {
+                                        loadingBar.dismiss();
+                                        // Toast.makeText(ChatActivity.this, "Message Sent Successfully...", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else
+                                    {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(GroupChatActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                                    }
+                                    userMessageInput.setText("");
+                                }
+                            });
+                        }
+                    }
+                });
+            }else{
+                loadingBar.dismiss();
+                Toast.makeText(this, "Select at least one image", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void SendMessage()
+    {
+        String messageText = userMessageInput.getText().toString();
+
+        if (TextUtils.isEmpty(messageText))
+        {
+            Toast.makeText(this, "first write your message...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+
+            String messageSenderRef = "Groups/"  + currentGroupName;
+            String messageReceiverRef = "Groups/" + currentGroupName ;
+
+
+            DatabaseReference userMessageKeyRef = RootRef.child("Groups")
+                    .child(currentGroupName).push();
+
+            String messagePushID = userMessageKeyRef.getKey();
+
+            Map messageTextBody = new HashMap();
+            messageTextBody.put("message", messageText);
+            messageTextBody.put("type", "text");
+            messageTextBody.put("from", currentUserID);
+            messageTextBody.put("fromName", currentUserName);
+            messageTextBody.put("to", currentGroupName);
+            messageTextBody.put("fromImage", currentUserImage);
+            messageTextBody.put("messageID", messagePushID);
+            messageTextBody.put("time", saveCurrentTime);
+            messageTextBody.put("date", saveCurrentDate);
+
+            Map messageBodyDetails = new HashMap();
+            messageBodyDetails.put(messageSenderRef + "/" + messagePushID, messageTextBody);
+            messageBodyDetails.put( messageReceiverRef + "/" + messagePushID, messageTextBody);
+
+            RootRef.updateChildren(messageBodyDetails).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task)
+                {
+                    if (task.isSuccessful())
+                    {
+                        //Toast.makeText(GroupChatActivity.this, "Message Sent Successfully...", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(GroupChatActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                    userMessageInput.setText("");
+                }
+            });
+        }
+    }
 }
